f7f3eb4dc77a6cbd7e81d89710d2d79ba9f7f11b
matthew <matthew@matthews-MacBook-Pro.local>
2022-01-31 14:46:03 -0500

    testing modified endpoint

--- routes/api/coder.js
+++ routes/api/coder.js
@@ -3,25 +3,7 @@ const router = express.Router();
 const Clip = require("../../models/Clip");
 const { check, validationResult } = require("express-validator");
 
-router.post("/post", [
-    check("videoSrc").notEmpty().isString(),
-    check("market").notEmpty().isString(),
-    check("station").notEmpty().isString(),
-    check("title").notEmpty().isString(),
-    check("snippet").notEmpty().isString(),
-    check("coder").notEmpty().isString(),
-    check("seek").notEmpty().isString(),
-    check("start").notEmpty().isNumeric(),
-    check("stop").notEmpty().isNumeric(),
-], async(req, res) => {
-    const schemaValidationErrors = validationResult(req);
-    console.log(":::SchemaValidation:::", schemaValidationErrors);
-
-    if(!schemaValidationErrors.isEmpty()) {
-        return res.status(404).send({
-            message: "The inputs you entered to register are invalid."
-        })
-    }
+router.post("/post", async(req, res) => {
 
     Clip.findOne({_id: req.body._id}).then(clip => {
         if(clip) {
