8b3301f7310ac9ec6153b05e625757585231a8d6
matthew <matthew@matthews-MacBook-Pro.local>
2022-02-18 10:20:55 -0500

    removed schema validators

--- routes/api/coder.js
+++ routes/api/coder.js
@@ -4,26 +4,7 @@ const Clip = require("../../models/Clip");
 const { check, validationResult } = require("express-validator");
 
 router.post("/post",[
-    check("videoSrc").notEmpty().isString(),
-    check("station").notEmpty().isString(),
-    check("market").notEmpty().isString(),
-    check("title").notEmpty().isString(),
-    check("snippet").notEmpty().isString(),
-    check("coder").notEmpty().isString(),
-    check("seek").notEmpty().isString(),
-    check("start").notEmpty().isNumeric(),
-    check("stop").notEmpty().isNumeric(),
-    check("id").notEmpty().isNumeric(),
 ], async(req, res) => {
-
-    const schemaValidationErrors = validationResult(req);
-    console.log(":::SchemaValidation", schemaValidationErrors);
-
-    if(!schemaValidationErrors.isEmpty()) {
-        return res.status(404).send({
-            message: "The inputs you entered to register are invalid."
-        })
-    }
 
     Clip.findOne({_id: req.body._id}).then(clip => {
         if(clip) {
